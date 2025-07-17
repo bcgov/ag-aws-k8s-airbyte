ffd9a7064d9b66ec3f38fddcb0ed37b97c7e0126
NikhilMM89 <mmnikhil89@gmail.com>
2025-07-17 13:38:50 -0700

    adding the pipeline changes

---
+++ terraform-bootstrap/variables.tf
@@ -0,0 +1,14 @@
+variable "backend_bucket_name" {
+  description = "Name of the S3 bucket for Terraform remote state"
+  type        = string
+}
+
+variable "dynamodb_table_name" {
+  description = "Name of the DynamoDB table for state locking"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment (e.g. dev, prod)"
+  type        = string
+}
