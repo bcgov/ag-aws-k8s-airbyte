0ea10d4b8f0add6dbfd505015d007f7ba46fcff6
NikhilMM89 <mmnikhil89@gmail.com>
2025-07-22 21:16:48 -0700

    adding the changes

--- Terraform/eks.tf
+++ Terraform/eks.tf
@@ -2,7 +2,7 @@ resource "aws_eks_cluster" "this" {
   name     = var.cluster_name
   role_arn = aws_iam_role.eks_cluster_role.arn
 
-  version = "1.29"
+  version = "1.33"
 
   vpc_config {
     subnet_ids = var.subnet_ids
