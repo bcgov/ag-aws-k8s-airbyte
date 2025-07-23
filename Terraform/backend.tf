b6f5f26bb65b854f3f6d15616d2031d83fc400f4
NikhilMM89 <117143852+NikhilMM89@users.noreply.github.com>
2025-07-22 19:56:11 -0700

    Update backend.tf

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraformstate"
+    key            = "eks/dev/terraform.tfstate"
+    region         = "ca-central-1"
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
+  }
+}
 
 
 
